e8337995b575f8aef36d2bb2aa8a24acf183f16a
Woolooloo-Mage <134153216+Woolooloo-Mage@users.noreply.github.com>
2023-05-28 21:55:04 -0400

    Update index.js

--- source/index.js
+++ source/index.js
@@ -1,8 +1,69 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { Token } = require('./config.json');
 
+const client = new Client({ intents: [
+  GatewayIntentBits.Guilds,
+  GatewayIntentBits.GuildMessages,
+  GatewayIntentBits.MessageContent,
+] });
 
+client.commands = new Collection();
 
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+  // Set a new item in the Collection with the key as the command name and the value as the exported module
+  
+	if ('data' in command && 'execute' in command) { // check for command 
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
 
+client.on(Events.InteractionCreate, async interaction => {   // start of redirect
+	if (!interaction.isChatInputCommand()) return;
 
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});                                                            // end of redirect
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! I'm logged in to the ${c.user.tag}`);
+});
+// Login message
+
+client.on('messageCreate', (message) => {  // rules
+  if (message.author.bot) {
+    return;
+  }
+  
+  if (message.content === 'Hey!') {
+    message.reply('Hey!');
+  }
+});
+
+// Log in to Discord with your client's token
+client.login( Token );
 
 
